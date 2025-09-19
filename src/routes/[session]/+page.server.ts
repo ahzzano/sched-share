7d7359f04ae932ee5e69a3ecc961c3a21eddb441
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-09-19 21:54:28 +0800

    feat: update .gitignore

--- src/routes/[session]/+page.server.ts
+++ src/routes/[session]/+page.server.ts
@@ -1,6 +1,16 @@
+import { db } from "$lib/server/db"
+import { groups } from "$lib/server/db/schema"
+import { eq } from "drizzle-orm"
 import type {PageLoad} from "./$types"
+import { error } from "@sveltejs/kit"
 
-export const load: PageLoad = ({params}) => {
+export const load: PageLoad = async ({params}) => {
+    const group = await db.select().from(groups).where(eq(groups.id, parseInt(params.session)))
+    if (group.length == 0) {
+        return error(404, {
+            message: 'Group not found'
+        })
+    }
     return {
         session: params.session
     }
