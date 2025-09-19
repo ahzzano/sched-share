da1c6d670952b98d3e2e062ce3037eb5e1093dcc
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-09-19 10:43:41 +0800

    feat: add schemas to project

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,25 @@
 import { pgTable, serial, integer, text } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', {
+export const users = pgTable('user', {
 	id: serial('id').primaryKey(),
-	name: text('name')
+	name: text('name'),
+    group: serial('group_id').references(() => groups.id)
 });
+
+export const groups = pgTable('group', {
+    id: serial('id').primaryKey(),
+    name: text('name'),
+})
+
+export const schedules = pgTable('schedules', { 
+    id: serial('id'),
+    user: serial('user_id').references(() => users.id).notNull()
+})
+
+export const items = pgTable('items', {
+    name: serial('id'),
+    schedule: serial('schedule_id').references(() => schedules.id).notNull(),
+    start: text('start-time'),
+    end: text('start-time'),
+    days: text('days') 
+})
