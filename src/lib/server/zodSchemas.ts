f45f83c0171a7f5ef987787954e12ff2681cba84
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-09-20 22:28:21 +0800

    feat: add zod schema for items

--- src/lib/server/zodSchemas.ts
+++ src/lib/server/zodSchemas.ts
@@ -5,3 +5,17 @@ export const userSchema = z.object({
     password: z.string().nullable().default(null),
     group: z.number()
 })
+
+export const itemSchema = z.object({
+    name: z.string().nonempty(),
+    startTime: z.string().nonempty(),
+    endTime: z.string().nonempty(),
+
+    sunday: z.boolean(),
+    monday: z.boolean(),
+    tuesday: z.boolean(),
+    wednesday: z.boolean(),
+    thursday: z.boolean(),
+    friday: z.boolean(),
+    saturday: z.boolean(),
+})
