72032fdd9b3bea60058c3a835fb49832a4634c58
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-09-20 23:50:53 +0800

    feat: add cards

--- src/routes/[session]/+page.svelte
+++ src/routes/[session]/+page.svelte
@@ -31,71 +31,73 @@ Add Schedule
 
 <div class="flex gap-5 p-5">
     {#each users as user}
-        <div class="p-4 border-black border-2 rounded-2xl">
-            <div class="flex flex-fil">
-                <span class="grow-1">{user.name}</span>
-                <form action="?/deleteUser" method="POST" use:enhance={() => {
-                    return async ({ update }) => {
-                        invalidateAll()
-                        await update()
-                    }
-                }}>
-                    <input type="hidden" name="id" value={user.id}/>
-                    <button>Delete</button>
-                </form>
-            </div>
-            {#each user.items as item}
-                <div class="flex gap-2">
-                    <span class="grow-0">{item.name}</span>
-                    <span class="grow-1">{item.start}-{item.end}</span>
-
-                    <form 
-                        class="grow-2"
-                        action="?/deleteSchedule" 
-                        method="POST" 
-                        use:enhance={() => {
-                            return async ({ update }) => {
-                                invalidateAll()
-                                await update()
-                            }
-                        }}>
-                        <input type="hidden" name="id" value={item.id}/>
+        <div class="card bg-base-100 shadow-sm">
+            <div class="card-body">
+                <div class="card-title">
+                    <span class="grow-1">{user.name}</span>
+                    <form action="?/deleteUser" method="POST" use:enhance={() => {
+                        return async ({ update }) => {
+                            invalidateAll()
+                            await update()
+                        }
+                    }}>
+                        <input type="hidden" name="id" value={user.id}/>
                         <button>Delete</button>
                     </form>
                 </div>
-            {/each}
-            <form
-                method="POST"
-                action="?/addItemToSchedule"
-                use:enhance={() => {
-                    return async ({ update }) => {
-                        invalidateAll()
-                        await update()
-                    }
-                }}
-                class="flex flex-col gap-2"
-            >
-                <input type="hidden" name="userId" value={user.id}/>
+                {#each user.items as item}
+                    <div class="flex gap-2">
+                        <span class="grow-0">{item.name}</span>
+                        <span class="grow-1">{item.start}-{item.end}</span>
 
-                <input type="text" name="itemName" class="input" placeholder="Add Item"/>
-                <div class="flex gap-2">
-                    <div class="input">
-                        <span class="label">Start</span>
-                        <input type="time" name="startTime" required />
+                        <form 
+                            class="grow-2"
+                            action="?/deleteSchedule" 
+                            method="POST" 
+                            use:enhance={() => {
+                                return async ({ update }) => {
+                                    invalidateAll()
+                                    await update()
+                                }
+                            }}>
+                            <input type="hidden" name="id" value={item.id}/>
+                            <button>Delete</button>
+                        </form>
                     </div>
-                    <div class="input">
-                        <span class="label">End</span>
-                        <input type="time" name="endTime" required />
+                {/each}
+                <form
+                    method="POST"
+                    action="?/addItemToSchedule"
+                    use:enhance={() => {
+                        return async ({ update }) => {
+                            invalidateAll()
+                            await update()
+                        }
+                    }}
+                    class="flex flex-col gap-2"
+                >
+                    <input type="hidden" name="userId" value={user.id}/>
+
+                    <input type="text" name="itemName" class="input" placeholder="Add Item"/>
+                    <div class="flex gap-2">
+                        <div class="input">
+                            <span class="label">Start</span>
+                            <input type="time" name="startTime" required />
+                        </div>
+                        <div class="input">
+                            <span class="label">End</span>
+                            <input type="time" name="endTime" required />
+                        </div>
+                    
                     </div>
-                
-                </div>
 
-                <Daypicker/>
+                    <Daypicker/>
 
-                <button>
-                    Add Item
-                </button>
-            </form>
+                    <button>
+                        Add Item
+                    </button>
+                </form>
+            </div>
 
         </div>
     {/each}
