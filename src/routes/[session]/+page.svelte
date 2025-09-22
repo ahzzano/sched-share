c1adb08dfa3ef59890ac70412418192a83161a11
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-09-22 12:30:47 +0800

    feat: add trash icon

--- src/routes/[session]/+page.svelte
+++ src/routes/[session]/+page.svelte
@@ -41,9 +41,9 @@
     </form>
 </div>
 
-<div class="grid grid-cols-1 md:grid-cols-3 items-start p-8 md:p-12 gap-4">
+<div class="grid grid-cols-1 md:grid-cols-4 items-start p-8 md:p-12 gap-4">
     {#each users as user}
-        <div class="card bg-base-100 shadow-sm md:w-128">
+        <div class="card bg-base-100 shadow-sm md:w-90">
             <div class="card-body">
                 <div class="card-title">
                     <span class="grow-1">{user.name}</span>
@@ -58,7 +58,18 @@
                         }}
                     >
                         <input type="hidden" name="id" value={user.id} />
-                        <button>Delete</button>
+                        <button
+                            class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 640 640"
+                                class="w-4 h-4"
+                                ><!--!Font Awesome Free v7.0.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path
+                                    d="M232.7 69.9L224 96L128 96C110.3 96 96 110.3 96 128C96 145.7 110.3 160 128 160L512 160C529.7 160 544 145.7 544 128C544 110.3 529.7 96 512 96L416 96L407.3 69.9C402.9 56.8 390.7 48 376.9 48L263.1 48C249.3 48 237.1 56.8 232.7 69.9zM512 208L128 208L149.1 531.1C150.7 556.4 171.7 576 197 576L443 576C468.3 576 489.3 556.4 490.9 531.1L512 208z"
+                                /></svg
+                            >
+                        </button>
                     </form>
                 </div>
 
